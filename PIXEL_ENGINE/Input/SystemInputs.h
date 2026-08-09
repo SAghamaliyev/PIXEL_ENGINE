85c908d59417e0a992a48ef9332ff22b8d00ea00
Said Aghamaliyev <digitalwarior666@gmail.com>
2026-08-09 22:46:22 +0400

    removed useless folder in input/

--- PIXEL_ENGINE/Input/SystemInputs.h
+++ PIXEL_ENGINE/Input/SystemInputs.h
@@ -1,6 +1,6 @@
 #include <glad/glad.h> 
 #include <GLFW/glfw3.h>
 
-#include "../InputManager.h"
+#include "InputManager.h"
 
 void callSystemInputs(GLFWwindow* window);
--- PIXEL_ENGINE/main.cpp
+++ PIXEL_ENGINE/main.cpp
@@ -2,7 +2,7 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 #include <string>
-#include "Input/SystemInput/SystemInputs.h"
+#include "Input/SystemInputs.h"
 
 using namespace std;
 
